0b7ec0461b5e65f2b9d7f86e6ccce1765470c52d
Harriet C Kiyai <kharie65@gmail.com>
2019-05-01 16:06:03 +0200

    Added functions to buttons

---
+++ myjquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#mannen").click(function() {
+    $(".man").css({ "background-color": "green" });
+  });
+  $("#vrouwen").click(function() {
+    $(".vrouw").css({ "background-color": "blue" });
+  });
+});
