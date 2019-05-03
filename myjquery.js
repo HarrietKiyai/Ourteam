9c577e9beff5b18f78f5d8f48a7fbb6a58c8a878
Harriet C Kiyai <kharie65@gmail.com>
2019-05-03 09:58:25 +0200

    toggle method trial

--- myjquery.js
+++ myjquery.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   $("#mannen").click(function() {
     $(".man").css({ "background-color": "green" });
+    $("#mannen").toggle();
   });
   $("#vrouwen").click(function() {
     $(".vrouw").css({ "background-color": "blue" });
+    if (display === true) {
+      $("#vrouwen").show();
+    } else if (display === false) {
+      $("#vrouwen").hide();
+    }
   });
-  // comment
 });
