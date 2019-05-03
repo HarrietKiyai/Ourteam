5cbd1fb507af1ef0a9133b54121c6c2c5c0986fe
Harriet C Kiyai <kharie65@gmail.com>
2019-05-03 14:20:58 +0200

    added member html and css flies

--- myjquery.js
+++ myjquery.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
   $("#mannen").click(function() {
     $(".man").css({ "background-color": "green" });
-    $("#mannen").toggle();
   });
   $("#vrouwen").click(function() {
     $(".vrouw").css({ "background-color": "blue" });
-    if (display === true) {
-      $("#vrouwen").show();
-    } else if (display === false) {
-      $("#vrouwen").hide();
-    }
   });
 });
