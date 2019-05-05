17bf3f2304cfbf6c363a2c7aa02b4741226e3938
Harriet C Kiyai <kharie65@gmail.com>
2019-05-05 22:22:07 +0200

    Added jquery fuctionality and updated the footer element

--- myjquery.js
+++ myjquery.js
@@ -1,8 +1,26 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $("#mannen").click(function() {
     $(".man").css({ "background-color": "green" });
   });
-  $("#vrouwen").click(function() {
+  /*$("#vrouwen").click(function() {
     $(".vrouw").css({ "background-color": "blue" });
+  });
+});*/
+
+// Loop update
+$(document).ready(function() {
+  $("#mannen").click(function() {
+    if ($(".man").hasClass("green")) {
+      $(".man").removeClass("green");
+    } else {
+      $(".man").addClass("green");
+    }
+  });
+  $("#vrouwen").click(function() {
+    if ($(".vrouw").hasClass("blue")) {
+      $(".vrouw").removeClass("blue");
+    } else {
+      $(".vrouw").addClass("blue");
+    }
   });
 });
